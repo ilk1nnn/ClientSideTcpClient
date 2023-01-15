5f63ba884a8dd7b8d1ca22f6e77ca793a13c31c5
Sule_jr92 <Sule_jr92@STHQ012D-02>
2023-01-15 16:27:27 +0400

    Add project files.

---
+++ ClientSideTcpClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSideTcpClient
+{
+    public class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var client = new TcpClient();
+            var ip = IPAddress.Parse("10.2.13.36");
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+            try
+            {
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    var writer = Task.Run(() =>
+                    {
+                        while (true)
+                        {
+                            var text = Console.ReadLine();
+                            var stream = client.GetStream();
+                            var bw = new BinaryWriter(stream);
+                            bw.Write(text);
+
+                        }
+                    });
+
+                    var reader = Task.Run(() =>
+                    {
+                        while (true)
+                        {
+
+                            var stream = client.GetStream();
+                            var br = new BinaryReader(stream);
+                            Console.WriteLine($"From Server {br.ReadString()}");
+                        }
+                    });
+                    Task.WaitAll(writer, reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
